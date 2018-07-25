79bae3dc89d3da510b3bfd0f87470beac8f46b02
Roberto Garza <robertg4567@hotmail.com>
2018-07-25 16:14:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,9 +29,9 @@ class Song
     @@genres.each do |genre|
       if genre_count[genre]
         genre_count[genre] +=1
-      else 
+      else
         genre_count[genre] = 1
-      end 
+      end
     end
   end
 
